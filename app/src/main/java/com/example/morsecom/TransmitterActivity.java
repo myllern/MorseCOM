86b21fd5c99fb11d20ca896a3ba5cafdaef46935
myllern <ax2685mu-s@student.lu.se>
2023-01-23 15:19:05 +0100

    Test

---
+++ app/src/main/java/com/example/morsecom/TransmitterActivity.java
@@ -0,0 +1,14 @@
+package com.example.morsecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransmitterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transmitter);
+    }
+}
