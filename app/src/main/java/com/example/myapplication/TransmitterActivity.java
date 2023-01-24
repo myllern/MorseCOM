abb874c6d31fd4086c435598873e7080ced3548f
myllern <49235951+myllern@users.noreply.github.com>
2023-01-24 20:16:34 +0100

    Translator/libary added

--- app/src/main/java/com/example/myapplication/TransmitterActivity.java
+++ app/src/main/java/com/example/myapplication/TransmitterActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 public class TransmitterActivity extends AppCompatActivity {
     Button sendButton;
     EditText messageBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +27,16 @@ public class TransmitterActivity extends AppCompatActivity {
         messageBox = (EditText) findViewById(R.id.transmitter_input_box);
 
 
-
     }
 
     public void getMessageInput(){
         if(messageBox.getText().toString().isEmpty()){
-            Toast.makeText(getApplicationContext(), "Enter the Data", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Enter Data", Toast.LENGTH_SHORT).show();
+  //        Log.d("myTag", "This is my message");
         }
+
+        Log.d("Transmitter", messageBox.getText().toString());
+
 
     }
 
